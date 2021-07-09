496d208397d52910fc1b567b9694545e5f65dc80
paulaLopes2270 <plnhschltz@gmail.com>
2021-07-09 16:15:31 -0300

    componentes e props

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 import C1 from './componentes/comp1';
-export default function App1(){
+
+export default function App1() {
   return (
-    <View>
-      <C1/>
-      <C1/>
-      <C1/>
-      <C1/>
+    <View style={estilos.container}>
+      <C1 curso="Rect Native" cor="#ff0" />
+      <C1 curso="c++" cor="#f0f" />
+      <C1 curso="Javascript" cor="#0ff" />
+      <C1 curso="Rect Native" cor="#000" />
+      <Text style={estilos.txt2}>Youtube</Text>
     </View>
   );
-};
+}
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#444',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  txt2: {
+    color: '#f500d4',
+    fontSize: 35,
+  },
+});
--- componentes/comp1.js
+++ componentes/comp1.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import {Text} from 'react-native';
-export default function(){
-  return(
-      <Text>CFB Cursos</Text>
+import {StyleSheet, Text} from 'react-native';
+
+
+export default function (props) {
+  let cor=props.cor;
+  return (
+    <Text style={(estilos.txt1, {color: cor})}>
+      CFB Cursos - Curso de {props.curso}
+    </Text>
   );
-};
+}
+
+const estilos = StyleSheet.create({
+  txt1: {
+    // color: '#f00',
+    fontSize: 20,
+  },
+});
